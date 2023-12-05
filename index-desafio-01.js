df9a77fa0826725f5606c6631c495b509cf0e41f
Zacher99 <matheus.zacher@outlook.com>
2023-12-05 14:33:18 -0300

    trocando o nome das variáveis para inglês

--- index-desafio-01.js
+++ index-desafio-01.js
@@ -1,15 +1,15 @@
 //Desafio do Herói
 
-let nomeHeroi = "João"
-let expHeroi = 10001
+let heroName = "João"
+let heroExp = 10001
 
-let eloHeroi = ""
-if(expHeroi<1000){eloHeroi = "Ferro";}
-else if(expHeroi<=2000){eloHeroi = "Bronze"}
-else if(expHeroi<=5000){eloHeroi = "Prata"}
-else if(expHeroi<=7000){eloHeroi = "Ouro"}
-else if(expHeroi<=8000){eloHeroi = "Platina"}
-else if(expHeroi<=9000){eloHeroi = "Ascendente"}
-else if(expHeroi<=10000){eloHeroi = "Imortal"}
-else (expHeroi>10001);{eloHeroi = "Radiante"}
- console.log("O herói de nome, " + nomeHeroi + ", está no nivel de " + eloHeroi);
+let heroRank = ""
+if(heroExp<1000){heroRank = "Ferro";}
+else if(heroExp<=2000){heroRank = "Bronze"}
+else if(heroExp<=5000){heroRank = "Prata"}
+else if(heroExp<=7000){heroRank = "Ouro"}
+else if(heroExp<=8000){heroRank = "Platina"}
+else if(heroExp<=9000){heroRank = "Ascendente"}
+else if(heroExp<=10000){heroRank = "Imortal"}
+else (heroExp>10001);{heroRank = "Radiante"}
+ console.log("O herói de nome, " + heroName + ", está no nivel de " + heroRank);
